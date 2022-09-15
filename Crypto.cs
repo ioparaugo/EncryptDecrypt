307c5c2f77e6d968f0094a63154fbd31e7a187dc
Ikenna <ike007007@gmail.com>
2022-09-15 14:31:21 +0400

    add crypto solution

---
+++ Crypto.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace EncryptDecryptTool
+{
+    public static class Crypto
+    {
+        public static string DESEncrypt(string strEncryptText)
+        {
+            string strEncryptedTxt = string.Empty;
+            string strDESSaltKey =  "#$%dso$4";
+
+            if (!string.IsNullOrWhiteSpace(strEncryptText) && !string.IsNullOrWhiteSpace(strDESSaltKey))
+            {
+                byte[] rgbIv = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+                var rgbKey = Encoding.UTF8.GetBytes(strDESSaltKey);
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+                byte[] inputArray = Encoding.UTF8.GetBytes(strEncryptText);
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(rgbKey, rgbIv), CryptoStreamMode.Write);
+                cs.Write(inputArray, 0, inputArray.Length);
+                cs.FlushFinalBlock();
+                strEncryptedTxt = Convert.ToBase64String(ms.ToArray());
+            }
+
+            return strEncryptedTxt;
+        }
+
+        public static string DESDecrypt(string strDecryptText)
+        {
+            string strDecryptedTxt = string.Empty;
+            string strDESSaltKey = "#$%dso$4";
+
+            if (!string.IsNullOrWhiteSpace(strDecryptText) && !string.IsNullOrWhiteSpace(strDESSaltKey))
+            {
+                byte[] rgbIv = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+                var rgbKey = Encoding.UTF8.GetBytes(strDESSaltKey);
+                DESCryptoServiceProvider des = new DESCryptoServiceProvider();
+
+                byte[] inputByteArray = Convert.FromBase64String(strDecryptText);
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(rgbKey, rgbIv), CryptoStreamMode.Write);
+                cs.Write(inputByteArray, 0, inputByteArray.Length);
+                cs.FlushFinalBlock();
+                Encoding encoding = Encoding.UTF8;
+                strDecryptedTxt = encoding.GetString(ms.ToArray());
+            }
+
+            return strDecryptedTxt;
+        }
+
+        public static string TripleDESEncrypt(string strEncryptText)
+        {
+            string strEncryptedTxt = string.Empty;
+            string strTripleDESSaltKey =  "sta!2021@vc#crm$v9.0%sau";
+
+            if (!string.IsNullOrWhiteSpace(strEncryptText) && !string.IsNullOrWhiteSpace(strTripleDESSaltKey))
+            {
+                byte[] rgbIv = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+                var rgbKey = Encoding.UTF8.GetBytes(strTripleDESSaltKey);
+                TripleDESCryptoServiceProvider des = new TripleDESCryptoServiceProvider();
+
+                byte[] inputArray = Encoding.UTF8.GetBytes(strEncryptText);
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(rgbKey, rgbIv), CryptoStreamMode.Write);
+                cs.Write(inputArray, 0, inputArray.Length);
+                cs.FlushFinalBlock();
+                strEncryptedTxt = Convert.ToBase64String(ms.ToArray());
+            }
+
+            return strEncryptedTxt;
+        }
+
+        public static string TripleDESDecrypt(string strDecryptText)
+        {
+            string strDecryptedTxt = string.Empty;
+            string strTripleDESSaltKey = "sta!2021@vc#crm$v9.0%sau";
+
+            if (!string.IsNullOrWhiteSpace(strDecryptText) && !string.IsNullOrWhiteSpace(strTripleDESSaltKey))
+            {
+                byte[] rgbIv = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
+                var rgbKey = Encoding.UTF8.GetBytes(strTripleDESSaltKey);
+                TripleDESCryptoServiceProvider des = new TripleDESCryptoServiceProvider();
+
+                byte[] inputByteArray = Convert.FromBase64String(strDecryptText);
+                MemoryStream ms = new MemoryStream();
+                CryptoStream cs = new CryptoStream(ms, des.CreateDecryptor(rgbKey, rgbIv), CryptoStreamMode.Write);
+                cs.Write(inputByteArray, 0, inputByteArray.Length);
+                cs.FlushFinalBlock();
+                Encoding encoding = Encoding.UTF8;
+                strDecryptedTxt = encoding.GetString(ms.ToArray());
+            }
+
+            return strDecryptedTxt;
+        }
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EncryptDecryptTool
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            valueTxt.Text = "apiadminuser|J!J@R#I$H%K^F&R*2021|20210817"; //default value
+        }
+
+
+        private void btnDecrypt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(valueTxt.Text))
+                {
+                    resultTxt.Text = Crypto.TripleDESDecrypt(valueTxt.Text);
+                }
+            }
+            catch(Exception ex)
+            {
+                resultTxt.Text = ex.Message;
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(valueTxt.Text))
+                {
+                    resultTxt.Text = Crypto.TripleDESEncrypt(valueTxt.Text);
+                }
+            }
+            catch(Exception ex)
+            {
+                resultTxt.Text = ex.Message;
+            }
+        }
+
+        private void valueTxt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
